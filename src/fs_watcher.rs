49888e7abb5ca09b19231f20d2cb35cc043d8ef6
Dmitriy Rusov <rusov.d.s@gmail.com>
2025-03-23 15:27:10 +0100

    fix: revert debounce

--- src/fs_watcher.rs
+++ src/fs_watcher.rs
@@ -1,14 +1,20 @@
 use crate::controller::Controller;
 use notify::{EventKind, RecommendedWatcher, RecursiveMode, Watcher};
+use std::collections::HashMap;
 use std::path::Path;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use tokio::sync::mpsc;
+use tokio::task::JoinHandle;
+use tokio::time::{sleep, Duration};
 
 pub struct FsWatcher;
 
 impl FsWatcher {
     pub async fn watch(src_path: String, controller: Arc<Controller>) {
         let (tx, mut rx) = mpsc::channel(1);
+        let debounce_tasks: Arc<Mutex<HashMap<String, JoinHandle<()>>>> =
+            Arc::new(Mutex::new(HashMap::new()));
+
         let mut watcher = RecommendedWatcher::new(
             move |res| {
                 let _ = tx.blocking_send(res);
@@ -29,7 +35,23 @@ impl FsWatcher {
                 EventKind::Modify(_) | EventKind::Create(_) | EventKind::Remove(_)
             ) {
                 println!("📦 File system change detected at: {:?}", event.paths);
-                controller.on_fs_change(src_path.clone()).await;
+                let key = src_path.clone();
+                let ctrl = controller.clone();
+                let debounce_tasks = debounce_tasks.clone();
+
+                let key_for_task = key.clone();
+
+                if let Some(handle) = debounce_tasks.lock().unwrap().remove(&key) {
+                    handle.abort();
+                }
+
+                let handle = tokio::spawn(async move {
+                    sleep(Duration::from_millis(1000)).await;
+                    println!("✅ Debounced sync for {}", key_for_task);
+                    ctrl.on_fs_change(key_for_task).await;
+                });
+
+                debounce_tasks.lock().unwrap().insert(key, handle);
             }
         }
     }
